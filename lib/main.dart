f37d0e724ded86edc7383f2d67a9585ddd7efaa8
StandOut-0 <standout.first@gmail.com>
2024-07-04 13:55:23 +0900

    불변성을 유지하면서 클래스를 복사하기 copyWith

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,20 @@ import 'package:ticket_app/base/bottom_nav_bar.dart';
 
 void main() {
   runApp(const MyApp());
+  var test = TestClass(x: 4, y: 10);
+  print(test.x);
+  var newTest = test.copyWith(30, 40);
+  print(test.y);
+}
+
+class TestClass{
+  int x;
+  int y;
+
+  TestClass({required this.x, required this.y});
+  TestClass copyWith(int? x, int? y){
+  return TestClass(x:x??this.x, y:y??this.y);
+  }
 }
 
 class MyApp extends StatelessWidget {
