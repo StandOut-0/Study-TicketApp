da6e3b766f599e3883725f07f4ceec5263ad7a99
StandOut-0 <standout.first@gmail.com>
2024-07-04 14:24:59 +0900

    Expanded를 통해 MainAxisAlignment와 같은 효과주기

--- lib/base/widgets/ticket_view.dart
+++ lib/base/widgets/ticket_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/res/style/app_styles.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({super.key});
@@ -8,6 +10,32 @@ class TicketView extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return SizedBox(
       width: size.width*0.85,
+      height: 179,
+      child: Container(
+        margin: EdgeInsets.only(right: 16),
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("NYC",
+                  style: AppStyles.headLineStyle3.copyWith(color: Colors.white),),
+                Expanded(child: Container()),
+                Text("NYC",
+                  style: AppStyles.headLineStyle3.copyWith(color: Colors.white),)
+              ],
+            )
+          ],
+        ),
+        decoration: BoxDecoration(
+            color: AppStyles.ticketBlue,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(21),
+              topRight: Radius.circular(21)
+          )
+        ),
+      ),
     );
   }
 }
