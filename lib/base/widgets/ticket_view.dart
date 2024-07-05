0edc59f553ac961912bbdd7a72619b1b2e609de3
StandOut-0 <standout.first@gmail.com>
2024-07-05 10:44:13 +0900

    만들었던 레이아웃을 복사 / 원 두개를 만들어 티켓모양으로 만든다.

--- lib/base/widgets/ticket_view.dart
+++ lib/base/widgets/ticket_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:ticket_app/base/res/style/app_styles.dart';
 import 'package:ticket_app/base/widgets/big_dot.dart';
 
 import 'app_layoutBuilder_widget.dart';
+import 'big_circle.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({super.key});
@@ -13,13 +15,12 @@ class TicketView extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return SizedBox(
       width: size.width*0.85,
-      height: 179,
+      height: 189,
       child: Container(
         margin: const EdgeInsets.only(right: 16),
         child: Column(
           children: [
             Container(
-              margin: const EdgeInsets.only(right: 16),
               padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
                   color: AppStyles.ticketBlue,
@@ -74,6 +75,72 @@ class TicketView extends StatelessWidget {
                 ],
               ),
             ),
+            Container(
+              height: 20,
+              color: AppStyles.ticketOrange,
+              child: Row(
+                children: [
+                  const BigCircle(isRight: false),
+                  Expanded(child: Container()),
+                  const BigCircle(isRight: true)
+                ],
+              )
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                  color: AppStyles.ticketOrange,
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(21),
+                      bottomRight: Radius.circular(21)
+                  )
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("NYC",
+                        style: AppStyles.headLineStyle3.copyWith(color: Colors.white),),
+                      Expanded(child: Container()),
+                      const BigDot(),
+                      Expanded(child: Stack(
+                        children: [
+                          const SizedBox(
+                            height: 24,
+                            child: AppLayoutBuilderWidget(randomDivider: 6),
+                          ),
+                          Center(
+                              child: Transform.rotate(angle: 1.5,
+                                  child: const Icon(Icons.local_airport_rounded, color: Colors.white
+                                  )
+                              )
+                          )
+                        ],
+                      )),
+                      const BigDot(),
+                      Expanded(child: Container()),
+                      Text("LDN",
+                        style: AppStyles.headLineStyle3.copyWith(color: Colors.white),)
+                    ],
+                  ),
+                  const SizedBox(height: 3),
+                  Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("New-York",
+                        style: AppStyles.headLineStyle3.copyWith(color: Colors.white),),
+                      Expanded(child: Container()),
+                      Text("8H 30M",
+                        style: AppStyles.headLineStyle3.copyWith(color: Colors.white),),
+                      Expanded(child: Container()),
+                      Text("London",
+                        style: AppStyles.headLineStyle3.copyWith(color: Colors.white),)
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
