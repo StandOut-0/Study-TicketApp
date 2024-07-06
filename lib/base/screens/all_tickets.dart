1e57e122aa7cdfd1a6bd4dec83071c84c402095b
StandOut-0 <standout.first@gmail.com>
2024-07-07 07:58:54 +0900

    alltickets에는 전체 list를, 기존 home에는 take()을 통해 일정 부분만 가지고오도록한다.

--- lib/base/screens/all_tickets.dart
+++ lib/base/screens/all_tickets.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/utils/all_json.dart';
+
+import '../widgets/ticket_view.dart';
 
 class AllTickets extends StatelessWidget {
   const AllTickets({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("All Tickets")
+      ),
+      body: ListView(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+      children: ticketList.map((singleTicket)
+      => TicketView(ticket: singleTicket)).toList(),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/base/screens/home_screen.dart
+++ lib/base/screens/home_screen.dart
@@ -72,7 +72,7 @@ class HomeScreen extends StatelessWidget {
                   SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
                       child: Row(
-                        children: ticketList.map((singleTicket)
+                        children: ticketList.take(2).map((singleTicket)
                         => TicketView(ticket: singleTicket)
                       ).toList(),
                       )
